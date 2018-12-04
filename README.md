9b2164f2d45dbfc3784d17bd115338ff027e3f8c
潘梦琳 <panmenglin@icourt.cc>
2018-12-04 19:59:38 +0800

    小程序滚动选择

---
+++ README.md
@@ -0,0 +1,21 @@
+# mp-select
+
+小程序自定义滚动选择组件
+
+## 使用
+
+1. 安装
+
+```
+npm install --save mp-select
+```
+
+2. 引入
+
+```json
+{
+  "usingComponents": {
+    "mp-select": "/miniprogram_npm/mp-select/index"
+  }
+}
+```
---
+++ src/index.js
@@ -0,0 +1,95 @@
+Component({
+  properties: {
+    value: {
+      type: Array,
+      value: []
+    },
+    range: {
+      type: Array,
+      value: []
+    },
+    type: {
+      type: String,
+      value: 'select'
+    },
+    require: {
+      type: Boolean,
+      value: false
+    }
+  },
+  data: {
+    curValue: [],
+    curRange: [],
+  },
+  ready() {
+    this.setData({
+      curValue: this.properties.value
+    })
+
+    const curRange = this.properties.range
+
+    this.changeSelected(curRange)
+
+    // 单选必填默认值
+    if (this.properties.type !== 'multiple' && this.properties.require && (!this.properties.value || this.properties.value.length === 0 || !this.properties.value[0])) {
+      const initValue = {}
+      initValue.value = this.properties.range[0].value
+      this.selectItem(null, initValue)
+    }
+  },
+  methods: {
+    selectItem(e, scrollValue) {
+      const {value} = e ? e.currentTarget.dataset : scrollValue
+      let {curValue} = this.data
+
+      if (this.properties.type === 'multiple') {
+        if (curValue.indexOf(value) >= 0) {
+          curValue.splice(curValue.indexOf(value), 1)
+        } else {
+          curValue.push(value)
+        }
+      } else {
+        curValue = [value]
+      }
+
+      this.setData({
+        curValue
+      })
+
+      this.changeSelected()
+    },
+    changeSelected(range) {
+      const curRange = range || this.data.curRange
+      const {curValue} = this.data
+
+      curRange.map(item => {
+        if (curValue.indexOf(item.value) >= 0) {
+          item.selected = true
+        } else {
+          item.selected = false
+        }
+        return item
+      })
+
+      this.setData({
+        curRange
+      })
+    },
+    save() {
+      this.triggerEvent('saveevent', {
+        value: this.data.curValue
+      })
+    },
+    cancel() {
+      this.triggerEvent('cancelevent', {
+      })
+    },
+    bindChange(e) {
+      if (this.properties.type !== 'multiple') {
+        const scrollValue = {}
+        scrollValue.value = this.properties.range[e.detail.value[0]].value
+        this.selectItem(null, scrollValue)
+      }
+    }
+  }
+})
